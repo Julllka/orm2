4bbbdfbe500e6627bff204103eff4f92fae6d9ef
Julllka <2biletavleto@mail.ru>
2024-12-16 16:27:42 +0300

    Add files via upload

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -6,8 +6,8 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    obj_list = Student.objects.all()
-    context = { 'object_list': obj_list}
+    object_list = Student.objects.prefetch_related('teachers')
+    context = {'object_list': object_list}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
