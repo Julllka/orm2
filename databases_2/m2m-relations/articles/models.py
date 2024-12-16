c9d553b74ddbcffe01b306a19f15ffd7bae4b5e9
Julllka <2biletavleto@mail.ru>
2024-12-16 16:29:50 +0300

    Add files via upload

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -1,33 +1,41 @@
 from django.db import models
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
+class Scope(models.Model):
+    topic = models.CharField(max_length=25, verbose_name='Раздел')
 
     class Meta:
         verbose_name = 'Раздел'
         verbose_name_plural = 'Разделы'
 
     def __str__(self):
-        return self.name
-
+        return self.topic
 
 class Article(models.Model):
+
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    tags = models.ManyToManyField(Tag, related_name='articles', through='Scope')
+    scope = models.ManyToManyField(Scope, through='Scopeship')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
-        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
 
-class Scope(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='РАЗДЕЛ')
-    is_main = models.BooleanField(default=False, verbose_name='ОСНОВНОЙ')
+
+
+class Scopeship(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopeship')
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE)
+    is_main = models.BooleanField(u'Главная')
+
+    # class Meta:
+    #     verbose_name = 'Тематика статьи'
+    #     verbose_name_plural = 'Тематика статьи'
+
+    def __str__(self):
+        return f'{self.article}_{self.scope}'
